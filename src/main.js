9e99311986aaccbabab513f7d6937c04b3e7eb1b
Aryaman <aryamannig@gmail.com>
2024-12-29 03:50:26 +0530

    feature: fixed bugs

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
-console.log("Hello Worldl");
+const myLibrary = [];
+
+function book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+    const title = document.getElementById("title").value;
+    const author = document.getElementById("author").value;
+    const pages = document.getElementById("pages").value;
+    const read = document.getElementById("read").checked;
+    const newBook = new book(title, author, pages, read);
+    myLibrary.push(newBook);
+    displayLibrary();
+}
